eff79406bb2b620de76052ddffb04907810c03ea
Martin <martin.alvarez.kuglen@gmail.com>
2024-03-01 15:20:00 -0800

    started using pathlib, changed dir to dir_name as local variable

--- multiprocesstools/MultiProcessHelper.py
+++ multiprocesstools/MultiProcessHelper.py
@@ -5,6 +5,7 @@ import time
 import logging
 from typing import List, Dict
 from logging import Logger
+from pathlib import Path
 
 logger = logging.getLogger("MultiProcessTools")
 logger.setLevel(logging.DEBUG)
@@ -28,10 +29,10 @@ def create_log_StreamHandler(logger: Logger, handler_name: str) -> None:
     logger.addHandler(stream_handler)
 
 
-def create_log_FileHandler(logger: Logger, handler_name: str, log_file: str) -> None:
+def create_log_FileHandler(logger: Logger, handler_name: str, log_file: Path) -> None:
     logger.setLevel(logging.DEBUG)
     ## create a file handler ##
-    file_handler = logging.FileHandler(log_file)
+    file_handler = logging.FileHandler(log_file.as_posix())
     ## create a logging format ##
     formatter = logging.Formatter(
         "%(asctime)s:%(filename)s:%(funcName)s:%(name)s:%(levelname)s:%(message)s"
@@ -49,7 +50,6 @@ def create_log_FileHandler(logger: Logger, handler_name: str, log_file: str) -> 
 
 
 class MultiProcessHelper:
-
     PROCESS_FILE_EXTENTIONS = [".tmp", ".err", ".fin"]
     LOG_FILE_EXTENTIONS = [".log"]
     INSTANCE_FILE_EXTENTIONS = [".running", ".failed", ".finished"]
@@ -58,22 +58,20 @@ class MultiProcessHelper:
     def __init__(
         self,
         name: str,
-        output_root: str,
+        output_root: Path,
         logger_names: List[str],
     ):
         time.sleep(np.random.random())
         # Initialize variables, type checks, and create output directories
         self.name = name
         self.processes = {}
-        self.directories = {}
+        self.directories: Dict[str, list] = {}
         self.files: Dict[str, list] = {}
         self.loggers: Dict[str, Logger] = {}
         self.logger: Logger = logging.getLogger("MultiProcessTools")
         try:
-            working_directory = os.path.join(
-                os.path.abspath(output_root), "multiprocesstools"
-            )
-            os.makedirs(working_directory, exist_ok=True)
+            working_directory = output_root / name
+            working_directory.mkdir(parents=True, exist_ok=True)
             self.directories["working_directory"] = working_directory
         except:
             raise ValueError(f"{working_directory} is not a valid path")
@@ -91,12 +89,12 @@ class MultiProcessHelper:
         """Initialize the instance_number number"""
         self.create_directory("instances")
         instance_number = 0
-        valid_instance = False
-        while valid_instance == False:
+        success = False
+        while success == False:
             instance_number += 1
-            valid_instance = self.create_file(
+            success = self.create_file(
                 file_name=f"{self.name}_{instance_number}",
-                dir="instances",
+                dir_name="instances",
                 extentions=self.INSTANCE_FILE_EXTENTIONS,
             )
         self.instance_number = instance_number
@@ -110,13 +108,13 @@ class MultiProcessHelper:
         log_path = self.get_directory("logs")
         success = self.create_file(
             file_name=log_file,
-            dir="logs",
+            dir_name="logs",
             extentions=self.LOG_FILE_EXTENTIONS,
         )
         if not success:
             raise ValueError(f"Log file {log_file} already exists")
 
-        log_file = os.path.join(log_path, f"{self.name}_{self.instance_number}")
+        log_file = Path(log_path / f"{self.name}_{self.instance_number}")
 
         create_log_FileHandler(
             self.logger, f"file_handler{self.instance_number}", log_file
@@ -134,8 +132,8 @@ class MultiProcessHelper:
             logger.info(f"Created log file: {log_file}")
 
     def create_directory(self, dir_name):
-        path = os.path.join(self.directories["working_directory"], dir_name)
-        if os.path.exists(path):
+        path = Path(self.directories["working_directory"] / dir_name)
+        if path.exists():
             self.logger.info(f"Path already exists:\n\n{path} ")
             if dir_name not in self.directories:
                 self.directories[dir_name] = path
@@ -149,10 +147,10 @@ class MultiProcessHelper:
         else:
             self.logger.info(f"Creating path:\n\n{path}")
             assert dir_name not in self.directories.keys()
-            os.makedirs(path, exist_ok=True)
+            path.mkdir(parents=True, exist_ok=True)
             self.directories[dir_name] = path
 
-    def get_directory(self, dir_name):
+    def get_directory(self, dir_name) -> Path:
         if any(dir_name in i for i in self.directories.items()):
             if dir_name in self.directories.keys():
                 return self.directories[dir_name]
@@ -161,78 +159,69 @@ class MultiProcessHelper:
         else:
             raise ValueError(f"{dir_name} is not in self.directories")
 
-    def create_file(self, file_name: str, dir: str, extentions: List[str]) -> bool:
+    def create_file(self, file_name: str, dir_name: str, extentions: List[str]) -> bool:
         """
         Creates a temporary file with the given name.
         returns true if the file was created successfully, false if the file exists.
         """
-        path = self.get_directory(dir)
+        path = self.get_directory(dir_name)
         for extention in extentions:
-            if os.path.exists(os.path.join(path, file_name + extention)):
+            if Path(path / file_name + extention).is_file():
+                logger.warning(f"File {file_name + extention} already exists")
                 return False
-        file = os.path.join(path, file_name + extentions[0])
+
+        file = Path(path / file_name + extentions[0])
         try:
-            f = open(file, "x")
-            f.close()
-            self.files[dir].append(file)
+            with open(file, "x") as f:
+                f.close()
+            self.files[dir_name].append(file)
             logger.info(f"Created file: {file}")
             return True
         except FileExistsError:
             logger.warning(f"File {file} already exists")
             return False
-        except:
-            time.sleep(np.random.uniform(0.01, 0.5))
-            try:
-                f = open(file, "x")
-                f.close()
-                self.files[dir].append(file)
-                logger.info(f"Created file: {file}")
-                return True
-            except FileExistsError:
-                logger.warning(f"File {file} already exists")
-                return False
-            except Exception as e:
-                self.logger.error("Unexpected error:", e.with_traceback())
-                raise e.with_traceback()
+        except Exception as e:
+            self.logger.error("Unexpected error:", e.with_traceback())
+            raise e.with_traceback()
 
-    def update_file(self, file_name, dir, new_suffix) -> None:
+    def update_file(self, file_name, dir_name, new_suffix) -> None:
         try:
-            idx = self.files[dir].index(file_name)
+            idx = self.files[dir_name].index(file_name)
         except KeyError as e:
-            logger.error(f"{dir} not in self.files")
+            logger.error(f"{dir_name} not in self.files")
             logger.error(e.with_traceback())
-            raise ValueError(f"{dir} not in self.files\n\n{e.with_traceback()}")
+            raise ValueError(f"{dir_name} not in self.files\n\n{e.with_traceback()}")
         except ValueError as e:
-            logger.error(f"{file_name} not in {dir}")
+            logger.error(f"{file_name} not in {dir_name}")
             logger.error(e.with_traceback())
-            raise ValueError(f"{file_name} not in {dir}\n\n{e.with_traceback()}")
-        file = os.path.join(self.get_directory(dir), file_name)
-        current_suffix = self.files[dir][idx].split(".")[-1]
-        assert file.endswith(f".{current_suffix}")
-        if os.path.exists(file):
+            raise ValueError(f"{file_name} not in {dir_name}\n\n{e.with_traceback()}")
+        file = Path(self.get_directory(dir_name) / file_name)
+        current_suffix = self.files[dir_name][idx].split(".")[-1]
+        assert file.suffix == f".{current_suffix}"
+        if file.exists():
             self.logger.info(f"Updating file: {file}")
-            new_file = file.replace(f".{current_suffix}", f".{new_suffix}")
-            os.rename(file, new_file)
-            new_filename = self.files[dir][idx].replace(
+            new_file = file.with_suffix(f".{new_suffix}")
+            file.rename(new_file)
+            new_filename = self.files[dir_name][idx].replace(
                 f".{current_suffix}", f".{new_suffix}"
             )
-            self.files[dir][idx] = new_filename
+            self.files[dir_name][idx] = new_filename
         else:
             self.logger.error(f"{file} does not exist...")
 
-    def delete_file(self, file_name, dir) -> None:
-        path = self.get_directory(dir)
+    def delete_file(self, file_name, dir_name) -> None:
+        path = self.get_directory(dir_name)
         try:
-            idx = self.files[dir].index(file_name)
-            file_name = self.files[dir].pop(idx)
-            file = os.path.join(path, file_name)
+            idx = self.files[dir_name].index(file_name)
+            file_name = self.files[dir_name].pop(idx)
+            file = Path(path / file_name)
         except ValueError as e:
-            self.logger.error(f"{file_name} not in {dir}")
+            self.logger.error(f"{file_name} not in {dir_name}")
             self.logger.error(e.with_traceback())
-            raise ValueError(f"{file_name} not in {dir}\n\n{e.with_traceback()}")
-        if os.path.exists(file):
+            raise ValueError(f"{file_name} not in {dir_name}\n\n{e.with_traceback()}")
+        if file.exists():
             self.logger.info(f"Deleting file: {file}")
-            os.remove(file)
+            file.unlink()
         else:
             self.logger.info(f"{file} was already deleted or does not exist...")
 
@@ -244,7 +233,7 @@ class MultiProcessHelper:
         self.create_directory(process_dir)
         self.processes[process_name] = {
             "status": self.STATUS_STATES[0],
-            "dir": process_dir,
+            "dir_name": process_dir,
         }
         return True
 
@@ -261,7 +250,7 @@ class MultiProcessHelper:
 
     def create_process_file(self, process_name: str, file_name: str) -> bool:
         try:
-            process_dir = self.processes[process_name]["dir"]
+            process_dir = self.processes[process_name]["dir_name"]
         except KeyError as e:
             self.logger.error(f"{process_name} not in self.processes")
             self.logger.error(e.with_traceback())
@@ -270,7 +259,7 @@ class MultiProcessHelper:
             )
         success = self.create_file(
             file_name=file_name,
-            dir=process_dir,
+            dir_name=process_dir,
             extentions=self.PROCESS_FILE_EXTENTIONS,
         )
         if not success:
@@ -281,7 +270,7 @@ class MultiProcessHelper:
         self, process_name: str, file_name: str, status: str
     ) -> None:
         try:
-            process_dir = self.processes[process_name]["dir"]
+            process_dir = self.processes[process_name]["dir_name"]
         except KeyError as e:
             self.logger.error(f"{process_name} not in self.processes")
             self.logger.error(e.with_traceback())
@@ -299,7 +288,7 @@ class MultiProcessHelper:
 
         self.update_file(
             file_name=file_name,
-            dir=process_dir,
+            dir_name=process_dir,
             new_suffix=self.PROCESS_FILE_EXTENTIONS[status_idx],
         )
 
