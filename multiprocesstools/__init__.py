f4742e324c5762ec34a31e77c09327231d6e45ce
Panzax <martin.alvarez.kuglen@gmail.com>
2024-03-01 15:20:35 -0800

    added gpuinit

--- multiprocesstools/__init__.py
+++ multiprocesstools/__init__.py
@@ -1,3 +1,3 @@
 from .MultiProcessHelper import MultiProcessHelper
-from .RunTimeCounter import RunTimeCounter
-
+from .RunTimeCounter import RunTimeCounter, run_func_IO_loop, wait_until_file_exists
+from ._gpuinit import gpuinit
